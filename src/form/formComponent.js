648625f887c5220c3472064087cb2508561f1ba4
JB <jonblair10@hotmail.com>
2018-08-10 13:06:11 -0500

    Adding new items

--- src/form/formComponent.js
+++ src/form/formComponent.js
@@ -8,12 +8,22 @@ const FormComponent = props => (
         alert(props.text);
     }}>
         <h2>With Redux-Lazy</h2>
-        <div>
-            <textarea className="textarea" placeholder="Open the developer console and enter some text" cols="100" rows="10"
-                onChange={event => props.textAction({ text: event.target.value })}
-                value={props.text}
-            />
+        <br/>
+        <div className="textBody">
+            <div>
+                OnChange: <br/>
+                <textarea className="textarea" placeholder="Open the developer console and enter some text" cols="120" rows="10"
+                    onChange={event => props.textAction({ text: event.target.value })}
+                    value={props.text}
+                />
+            </div>
+            <br/>
+            <div className="onChangeDelay">
+                OnKeyUp with Delay: <br/>
+                <textarea placeholder="Open the developer console and enter some text" cols="120" rows="10"/>
+            </div>
         </div>
+        <br/>
         <div>
             <input type="submit" value="Submit" />
         </div>
